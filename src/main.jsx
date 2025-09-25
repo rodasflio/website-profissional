e16e065d1710f8039528157e0e0342b0c59af6c2
Marley <askellad4157@gmail.com>
2025-09-25 15:20:18 -0300

    feat: apply neomorphic UI/UX design to all components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './styles/GlobalStyles.css'; // O arquivo correto de estilos globais
+import AppWithRouter from './App.jsx';
+import './styles/theme/UIUX.css'; // Importando o novo arquivo de tema
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppWithRouter />
   </React.StrictMode>
 );
